ee2854c2c0c526365caa1c91d555313222659bf8
Alejandro Cantón <alexcantongarcia97@gmail.com>
2021-07-08 20:57:17 +0200

    First Commit

---
+++ components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = ({  }) => {
+    return (
+        <aside className="sidebar">
+            <p>Sidebar</p>
+        </aside>
+    )
+}
+
+export default Sidebar
