97e9ce1f6b030eee51dc6fd1962190dc21c5d5ae
alexcanDvlpr <34892786+alexcanDvlpr@users.noreply.github.com>
2021-07-11 12:28:46 +0000

    Deploying to gh-pages from @ alexcanDvlpr/alexcantongarcia@0eb20fd085cfd3bd35d79d114923335a847ad181 🚀

--- _next/static/XHDORM2jVhQvI3BZi2qaP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/146baac4184c086618c8.css","static/chunks/pages/index-18a8b59929684db2b2da.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
