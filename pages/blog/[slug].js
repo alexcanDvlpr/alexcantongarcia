8c484683b63e156d2d06fcde02ddaf6c91d67a36
Alejandro Cantón <alexcantongarcia97@gmail.com>
2021-08-16 18:26:13 +0200

    Show metadata from posts

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,18 +1,35 @@
 import MDXComponents from '../../components/MDXComponents'
 import { getFileBySlug, getFiles } from '../../lib/mdx'
 import Header from '../../components/Header/Header'
-import { Container } from '@chakra-ui/react'
+import { Box, Text, Avatar, Tag, Container } from '@chakra-ui/react'
 import { MDXRemote } from 'next-mdx-remote'
 
 const Post = ({ source, frontmatter }) => {
+    const { author = '', avatar = '', date = '', readingTime, tags, title = '' } = frontmatter
 
     let timeToRead = Math.round(frontmatter.readingTime.minutes)
     timeToRead = (timeToRead === 0) ? 1 : timeToRead
+
+    const printMinutes = (minutes) => {
+        return Math.trunc(minutes) + 1
+    }
 
     return (
         <div className="main">
             <Header showTitle={true} />
             <Container maxW="container.lg" style={{ marginTop: '78px' }}>
+                <Box>
+                    <Text as="p" fontSize="md">{author}</Text>
+                    <Text as="p" fontSize="md">{date}</Text>
+                    <Text as="p" fontSize="md">{title}</Text>
+                    <Text as="p" fontSize="md">{`${printMinutes(readingTime.minutes)} Minutos de lectura`}</Text>
+                    <Avatar name="Segun Adebayo" src={`/${avatar}`} />
+                    {
+                        tags.map(tag => (
+                            <Tag key={tag}>{tag}</Tag>
+                        ))
+                    }
+                </Box>
                 <MDXRemote {...source} components={MDXComponents} />
             </Container>
         </div >
