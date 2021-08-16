2e43f4b78489354bbb0c8e4de8a887fed492f22b
Alejandro Cantón <alexcantongarcia97@gmail.com>
2021-08-16 16:40:10 +0200

    Fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,14 @@
-import { ColorModeScript } from "@chakra-ui/react"
+/* eslint-disable @next/next/no-css-tags */
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
+import { ColorModeScript } from "@chakra-ui/react"
 import theme from "../styles/theme"
+
 export default class Document extends NextDocument {
     render() {
         return (
             <Html lang="en">
                 <Head>
+                    <link rel="stylesheet" href="../styles/global.css" />
                     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous" />
                 </Head>
                 <body>
