89c5c64eb3474bd61e43b7b4aef66fd4fb2d902d
Alejandro Cantón <alexcantongarcia97@gmail.com>
2021-08-16 17:12:15 +0200

    Fix globals.css in vercel

--- components/CustomImage/CustomImage.js
+++ components/CustomImage/CustomImage.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import { Image, Box } from "@chakra-ui/react"
 
 const CustomImage = (props) => {
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../styles/theme'
 import '../styles/globals.css'
-import '../styles/prism.css'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
