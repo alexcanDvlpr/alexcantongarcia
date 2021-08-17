7d16159d6f4c58801e3d725a62c99b74092b79e3
Alejandro Cantón <alexcantongarcia97@gmail.com>
2021-08-17 17:39:12 +0200

    Prueba de reading time con articulo real

--- components/YouTubeVideo/YouTubeVideo.js
+++ components/YouTubeVideo/YouTubeVideo.js
@@ -3,13 +3,15 @@ import { AspectRatio, Box } from "@chakra-ui/react"
 const YoutubeVideo = ({ youtubeId, youtubeTitle }) => {
 
     return (
-        <Box w="60%" alignItems="center" className="youtubeVideo_content">
-            <AspectRatio ratio={16 / 9}>
-                <iframe
-                    src={`https://www.youtube.com/embed/${youtubeId}`}
-                    title={youtubeTitle}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-            </AspectRatio>
+        <Box className="youtubeVideo-content">
+            <Box w="60%" alignItems="center" className="youtubeVideo_content">
+                <AspectRatio ratio={16 / 9}>
+                    <iframe
+                        src={`https://www.youtube.com/embed/${youtubeId}`}
+                        title={youtubeTitle}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                </AspectRatio>
+            </Box>
         </Box>
     )
 }
