f4db23ccb70b4e0bc6f7bfe640a62df94b3089cf
Alejandro Cantón <alexcantongarcia97@gmail.com>
2021-08-21 19:29:51 +0200

    Arreglar el Slider de empresas

--- components/sections/CompanySection/CompanySection.js
+++ components/sections/CompanySection/CompanySection.js
@@ -5,14 +5,14 @@ const CompanySection = () => {
         <div className="companySection">
             <Heading as="h3" size="xl" mt={8} color="gray.700">He trabajado en:</Heading>
             <div className="sliderCompanies">
-                <Link href="https://es.worldline.com/es/home.html" target="_blank" rel="noreferrer">
-                    <Image className="companyLogo" boxSize="250px" src="/images/companySection/worldline.png" alt="Worldline Iberia" />
+                <Link href="https://es.worldline.com/es/home.html" target="_blank" rel="noreferrer" className="companyLogo">
+                    <Image boxSize="250px" src="/images/companySection/worldline.png" alt="Worldline Iberia" />
                 </Link>
-                <Link href="https://www.gmv.com/es/" target="_blank" rel="noreferrer">
-                    <Image className="companyLogo" boxSize="250px" src="/images/companySection/logo_gmv.svg" alt="GMV Innovating Solutions" />
+                <Link href="https://www.gmv.com/es/" target="_blank" rel="noreferrer" className="companyLogo">
+                    <Image boxSize="250px" src="/images/companySection/logo_gmv.svg" alt="GMV Innovating Solutions" />
                 </Link>
-                <Link href="https://www.bluetab.net/es/" target="_blank" rel="noreferrer">
-                    <Image className="companyLogo" boxSize="250px" src="/images/companySection/bluetab.svg" alt="/Bluetab An IBM Company" />
+                <Link href="https://www.bluetab.net/es/" target="_blank" rel="noreferrer" className="companyLogo">
+                    <Image boxSize="250px" src="/images/companySection/bluetab.svg" alt="/Bluetab An IBM Company" />
                 </Link>
             </div>
         </div>
