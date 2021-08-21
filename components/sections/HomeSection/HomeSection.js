d14367efd897a5d7fb97d7d8387da20da9270e2d
Alejandro Cantón <alexcantongarcia97@gmail.com>
2021-08-21 19:45:47 +0200

    Arreglar la alineacion del texto en responsive

--- components/sections/HomeSection/HomeSection.js
+++ components/sections/HomeSection/HomeSection.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import { Heading, Text } from '@chakra-ui/react'
 import ScrollingIcon from '../../icons/ScrollingIcon'
+import { Container, Heading, Text } from '@chakra-ui/react'
 
 const HomeSection = () => {
   return (
@@ -8,13 +7,13 @@ const HomeSection = () => {
             <div className="homeImageContent">
                 <img src="/avatar/alex.jpg" alt="123" />
             </div>
-            <div className="homeTitleContent">
+            <Container className="homeTitleContent">
                 <Heading className="homeTitle" as="h1" size="3xl">Hola,</Heading>
                 <Heading className="homeTitle lastHomeTitle" as="h1" size="3xl">Soy Alex Cantón 👋</Heading>
                 <Text className="homeText" mt={2} fontSize="2xl">Desarrollador web en Madrid, España. Tengo experiencia aplicando soluciones tanto en Backend como en Frontend.</Text>
                 <Text className="homeText" fontSize="2xl" mt={2}>Durante este tiempo he trabajado con Spring Boot, JavaScript, TypeScript y Node.js,
                     y con distintas tecnologías como Docker y Docker Compose, AWS, CI/CD etc.</Text>
-            </div>
+            </Container>
             <div className="scrolling-icon">
                 <div className="icon">
                     <ScrollingIcon fillColor="#00c896" />
