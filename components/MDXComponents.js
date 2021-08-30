4da40d4625b36f9d6131271df049c2b8230fd195
Alejandro Cantón <alexcantongarcia97@gmail.com>
2021-08-30 21:09:25 +0200

    Cambiar tamaño de los parrafos y continuar con el articulo

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -12,7 +12,7 @@ export default {
   h4: (props) => <Heading as="h4" mt={8} size="xl" {...props} />,
   h5: (props) => <Heading as="h5" mt={8} size="lg" {...props} />,
   h6: (props) => <Heading as="h6" mt={8} size="md" {...props} />,
-  p: (props) => <Text fontSize="xl" lineHeight={5} my={4} {...props} />,
+  p: (props) => <Text fontSize="lg" lineHeight={5} my={4} {...props} />,
   img: (props) => <Image {...props} />,
   Separator,
   YoutubeVideo,
