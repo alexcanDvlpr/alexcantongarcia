7b75d9411849796d26355e7e5da36df601fc66b3
Alejandro Cantón <alexcantongarcia97@gmail.com>
2021-10-30 19:43:35 +0200

    Añadida la meta description

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ function MyApp ({ Component, pageProps }) {
     <ChakraProvider theme={theme} initialColorMode={theme.config.initialColorMode}>
       <Head>
         <title>Alex Cantón</title>
+        <meta name="description" content="Mi nombre es Alex Cantón y soy desarrollador web desde el 2017 en Madrid, España. Esta es mi web junto con mi blog en la que quiero compartir conocimiento, enseñar nuevas tecnologías y enseñar JavaScript desde 0 para gente que este estudiando o se este abriendo camino en el mundo laboral." />
       </Head>
       <Component {...pageProps} />
     </ChakraProvider>
