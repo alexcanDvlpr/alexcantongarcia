9b4da8e85e426978d66885e6fcff0ae6fa6c2459
Alejandro Cantón <alexcantongarcia97@gmail.com>
2021-10-30 19:05:39 +0200

    Alineados los margenes de todas las paginas para que sean iguales. Añadido los colores de las tags. Header responsive

---
+++ data/tags.js
@@ -0,0 +1,13 @@
+const TAGS = {
+  Git: '#f1502f',
+  GitHub: '#171515',
+  GitLab: '#e24329',
+  HTML: '#e34c26',
+  CSS: '#264de4',
+  JavaScript: '#F0DB4F',
+  Desarrollo: '#000',
+  Testing: '#14BD00',
+  Personal: '#0003A9'
+}
+
+module.exports = TAGS
