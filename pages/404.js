2dacf71e8b078263ccb6276d016f747a321d8c28
Alejandro Cantón <alexcantongarcia97@gmail.com>
2021-11-22 00:00:18 +0100

    Crear la pagina de 404

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,27 @@
-import { Heading } from '@chakra-ui/react'
+import AlexCantonHead from '../components/AlexCantonHead'
+import { Container, Box, Heading, Link, Image } from '@chakra-ui/react'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 
 const Custom404Page = () => {
   return (
-    <Heading as="h1" mt={3} size="4xl">Esta es la pantalla de Error</Heading>
+    <>
+      <AlexCantonHead title="Ups... | Alex Cantón" />
+      <div className="main">
+        <Header showTitle={true} />
+        <Container className="error404" as="section" maxW="container.lg" >
+          <Box className="errorText">
+            <Heading mb={4} as="h1" size="2xl">¡Vaya...!</Heading>
+            <Heading mb={2} as="h2" size="lg">Parece que esta página no era lo que esperabas. Prueba a ir a mi blog y volver a empezar</Heading>
+            <Link mt={4} href="https://www.alexcantongarcia.es/blog">
+              <Heading as="h2" size="lg">Ir al Blog</Heading>
+            </Link>
+          </Box>
+          <Image className="image404" src="/images/pages/404/404.webp" alt="404" />
+        </Container>
+      </div >
+      <Footer />
+    </>
   )
 }
 
